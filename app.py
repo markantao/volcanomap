f1f8cddb13d848f4b199ecac15fdb74c89e16b95
Mark Antao <35659231+markantao@users.noreply.github.com>
2021-10-23 11:15:46 -0400

    used a dataframe to add elevation data on popup

--- app.py
+++ app.py
@@ -4,14 +4,15 @@ import pandas
 data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
 
 map = folium.Map(location=[43.65405369289918, -79.38369434349275], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
 #zip function is used to iterate between two lists with one for loop. Distributes the items one by one.
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi I am a marker!", icon=folium.Icon(color="green")))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) + "m", icon=folium.Icon(color="green")))
 
 map.add_child(fg)
 
