50ee646d321881ba83abcfcefcbdd9596ee2dd75
Mark Antao <35659231+markantao@users.noreply.github.com>
2021-10-23 10:40:23 -0400

    added a popup marker

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import folium
-map = folium.Map(location=[43.65405369289918, -79.38369434349275], zoom_start=4, tiles="Stamen Terrain")
+map = folium.Map(location=[43.65405369289918, -79.38369434349275], zoom_start=6, tiles="Stamen Terrain")
+
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[43.2, -79.7], popup="Hi I am a marker!", icon=folium.Icon(color="green")))
+
+map.add_child(fg)
+
 map.save("Map1.html")
