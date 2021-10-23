3cb866eb28e0c354704298c336ab2f6d8934f3bd
Mark Antao <35659231+markantao@users.noreply.github.com>
2021-10-23 12:04:31 -0400

    add base colors for countries based on population

--- app.py
+++ app.py
@@ -23,6 +23,10 @@ for lt, ln, el in zip(lat, lon, elev):
     fg.add_child(folium.CircleMarker(location=[lt, ln], radius=10, popup=str(el) + "m", fill_color=color_maker(el)
     , color="black", fill_opacity=0.7))
 
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x: {"fillColor":"green" if x['properties']['POP2005'] < 10000000 
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
 map.add_child(fg)
 
 map.save("Map1.html")
