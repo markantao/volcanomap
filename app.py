ee94ab655ba39cd5ca81e410e6a861b3da24d7d7
Mark Antao <35659231+markantao@users.noreply.github.com>
2021-10-23 11:02:35 -0400

    used volcanoes csv file to create multiple popups

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+
 map = folium.Map(location=[43.65405369289918, -79.38369434349275], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[43.2, -79.7], popup="Hi I am a marker!", icon=folium.Icon(color="green")))
+
+#zip function is used to iterate between two lists with one for loop. Distributes the items one by one.
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi I am a marker!", icon=folium.Icon(color="green")))
 
 map.add_child(fg)
 
